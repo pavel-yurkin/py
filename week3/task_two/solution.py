610b434bdab4d53e5ab14a2307b0e4237b3f6701
Pavel Yurkin <wellize@wellize-20.04.01-lts>
2020-09-09 17:21:23 +0300

    week3 last task

--- week3/task_two/solution.py
+++ week3/task_two/solution.py
@@ -9,7 +9,7 @@ class CarBase:
     def get_photo_file_ext(self):
          try:
             photo = self.photo_file_name.split('.')
-            if len(photo) == 2:
+            if len(photo) == 2 and photo[0] != '':
                 value = '.' + photo[1]
                 if value == '.jpg' or value == '.jpeg' or value == '.png' or value == '.gif':
                     return value
@@ -22,7 +22,7 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, brand, passenger_seats_count, photo_file_name, carrying):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         self.brand = brand
         self.car_type = 'car'
         self.photo_file_name = photo_file_name
@@ -40,15 +40,25 @@ class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
         self.car_type = 'truck'
         try:
             if body_whl == '':
                 body_whl = '0.0x0.0x0.0'
             store = body_whl.split('x')
-            self.body_length = store[0]
-            self.body_width = store[1]
-            self.body_height = store[2]
+
+            try:
+                if len(store) > 3:
+                    raise ValueError
+                float(store[0])
+                float(store[1])
+                float(store[2])
+            except ValueError:
+                store = [0, 0, 0]
+
+            self.body_length = float(store[0])
+            self.body_width = float(store[1])
+            self.body_height = float(store[2])
         except(IndexError):
             self.body_whl = -1
 
@@ -60,10 +70,16 @@ class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
         self.car_type = 'spec_machine'
         self.extra = extra
 
+def check_for_float(carrying):
+    try:
+        float(carrying)
+    except ValueError:
+        return False
+    return True
 
 def get_car_list(csv_filename):
     car_list = []
@@ -74,16 +90,22 @@ def get_car_list(csv_filename):
             if len(row) != 7:
                 continue
 
+            if not check_for_float(row[5]):
+                continue
+
+            if row[1] == '':
+                continue
+
             if row[0] == 'car':
-                if Car(row[1], row[2], row[3], row[5]).get_photo_file_ext() != None and Car(row[1], row[2], row[3], row[5]).passenger_seats_count != -1:
-                    car_list.append(Car(row[1], row[2], row[3], row[5]))
+                if Car(row[1], row[3], row[5], row[2]).get_photo_file_ext() != None and Car(row[1], row[3], row[5], row[2]).passenger_seats_count != -1:
+                    car_list.append(Car(row[1], row[3], row[5], row[2]))
 
             if row[0] == 'truck':
                 if Truck(row[1], row[3], row[5], row[4]).get_photo_file_ext() != None:
                     car_list.append(Truck(row[1], row[3], row[5], row[4]))
 
             if row[0] == 'spec_machine':
-                if SpecMachine(row[1], row[3], row[5], row[6]).get_photo_file_ext() != None:
+                if SpecMachine(row[1], row[3], row[5], row[6]).get_photo_file_ext() != None and row[6] != '':
                     car_list.append(SpecMachine(row[1], row[3], row[5], row[6]))
     return car_list
 
